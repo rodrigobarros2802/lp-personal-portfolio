b8473560311d8d9fa5431167ad896cb855bb9ee0
Rodrigo Barros <rodrigobarros2802@gmail.com>
2023-08-07 20:59:58 -0300

    aplicando javascript no btn

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector(".btn-fixed");
+
+window.addEventListener ("scroll", function(event) {
+    if (window.scrollY == 0) {
+        botao.classList.remove("visible");
+    } else {
+        botao.classList.add("visible");
+    }
+})
